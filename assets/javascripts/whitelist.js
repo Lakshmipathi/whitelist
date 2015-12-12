536c285dcb08cd1973391492909ebf0635b29e5e
Lakshmipathi.G <Lakshmipathi.G@giis.co.in>
2015-12-13 00:40:50 +0530

    ex

--- assets/javascripts/whitelist.js
+++ assets/javascripts/whitelist.js
@@ -1,6 +1,7 @@
 (function () {
-Discourse.Markdown.whiteListTag('script');
-Discourse.Markdown.whiteListTag('script', 'src', /https:\/\/asciinema.org\/a\/3uj8c8f04q9434ffsvk4s2sbb.js/);
-Discourse.Markdown.whiteListTag('script', 'id','/asciicast-3uj8c8f04q9434ffsvk4s2sbb/');
+Discourse.Markdown.whiteListTag('script', 'src', '*');
+Discourse.Markdown.whiteListTag('script', 'id','*');
 Discourse.Markdown.whiteListTag('textarea');
 })();
+
+
