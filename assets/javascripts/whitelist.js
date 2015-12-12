76d5862c044b95fea8304d20a8ee484201576cc5
Lakshmipathi.G <Lakshmipathi.G@giis.co.in>
2015-12-12 14:48:33 +0530

    ex

--- assets/javascripts/whitelist.js
+++ assets/javascripts/whitelist.js
@@ -1,8 +1,4 @@
 (function () {
 Discourse.Markdown.whiteListTag('script');
-Discourse.Markdown.whiteListTag('script', 'type', 'text/javascript');
-Discourse.Markdown.whiteListTag('script', 'src', '*');
-Discourse.Markdown.whiteListTag('script', 'id', '*');
-Discourse.Markdown.whiteListTag('script', 'async'); 
 Discourse.Markdown.whiteListTag('textarea');
 })();
